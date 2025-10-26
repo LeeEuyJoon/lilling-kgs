fb9f0a29e725dcf0559b9c8f1576884ded517f1e
Luti <wns6619@gmail.com>
2025-10-27 02:37:13 +0900

    initial commit

---
+++ src/main/java/luti/kgs/LillingKgsApplication.java
@@ -0,0 +1,13 @@
+package luti.kgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LillingKgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LillingKgsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/luti/kgs/LillingKgsApplicationTests.java
@@ -0,0 +1,13 @@
+package luti.kgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LillingKgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
